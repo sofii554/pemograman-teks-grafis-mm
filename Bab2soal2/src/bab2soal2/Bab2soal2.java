d5fd079ca7268ebb3d0a5f9caf7401abdb8f98f4
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-07-18 17:58:22 +0700

    Add files via upload

---
+++ Bab2soal2/src/bab2soal2/Bab2soal2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal2;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Bab2soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan="Ayam Geprek";
+        String minuman="Jus jambu";
+        
+        System.out.println("Makanan Favorit Saya : " + makanan);
+        System.out.println("Minuman Favorit Saya : " + minuman);
+
+    }
+    
+}
