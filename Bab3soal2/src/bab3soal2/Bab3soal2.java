b633a1720d9f2ece7c224ddeae340a7087eb676e
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-07-19 09:35:32 +0700

    Add files via upload

---
+++ Bab3soal2/src/bab3soal2/Bab3soal2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Bab3soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NIM = 2110117000;
+        String Nama = "Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        int Usia = 19;
+        double TinggiBadan = 172;
+        
+        System.out.println("NIM : "+NIM);
+        System.out.println("Nama Mahasiswa : "+Nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Usia : "+Usia);
+        System.out.println("Tinggi : "+TinggiBadan);
+
+    }
+    
+}
