d61e354389d832505e590d9d60ca3a348351206d
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-07-19 09:34:19 +0700

    Add files via upload

---
+++ Bab3soal1/src/bab3soal1/Bab3soal1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal1;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Bab3soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int number = 9;
+       float angka = 8.99f;
+       double huruf = 'A';
+       boolean var4 = false;
+       String kata = "Hello World";
+       
+       System.out.println(number);
+              System.out.println(angka);
+              System.out.println(huruf);
+              System.out.println(var4);
+       System.out.println(kata);
+
+
+    }
+    
+}
