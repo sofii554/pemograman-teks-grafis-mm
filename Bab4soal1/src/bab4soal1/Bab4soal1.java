b39a5f6d632d29cfa806c3f242d1927c64f117b4
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-08-27 20:20:26 +0700

    Add files via upload

---
+++ Bab4soal1/src/bab4soal1/Bab4soal1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal1;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Bab4soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 3;
+        int c = a+b;
+        System.out.println(c);
+      
+        int k = 10;
+        int l = 3;
+        int m = a-b;
+        System.out.println(m);
+        
+        int s = 15;
+        int i = 2;
+        int z = a*b;
+        System.out.println(z);
+        
+        int j = 10;
+        int f = 3;
+        int r = a/b;
+        System.out.println(r);
+    }
+    
+}
