e39f74193a530327c94cd6183faebafc33403d68
sofii554 <144987548+sofii554@users.noreply.github.com>
2024-08-29 08:37:21 +0700

    Add files via upload

---
+++ Bab7soal1/src/bab7soal1/Bab7soal1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab7soal1;
+
+/**
+ *
+ * @author Sheva
+ */
+public class Bab7soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+        }
+    }
+    
+}
